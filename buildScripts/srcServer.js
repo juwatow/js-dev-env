68390edcc6e5e600885d36e27e7b91b81d737463
Jeanne <jeanneu@careercruising.com>
2017-06-01 16:13:50 -0400

    HTTP Calls

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users',  function(req, res) {
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName":"Smith", "email":"bob.smith@gmail.com" },
+    {"id": 2, "firstName": "Jane", "lastName":"Smith", "email":"jane.smith@gmail.com" },
+    {"id": 3, "firstName": "Sarah", "lastName":"Lee", "email":"sarah.lee@gmail.com" }
+  ]);
+});
+
 app.listen(port, function(err){
   if(err) {
     console.log(err);
